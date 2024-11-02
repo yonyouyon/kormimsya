2753c5bf77b671baf7071e71fba6a0dd31f5baf8
YONSAN <112881127+yonyouyon@users.noreply.github.com>
2024-11-02 15:25:31 +1000

    try to fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
-import { useTelegram } from "../../hooks/useTelegram";
+import { useTelegram } from "../hooks/useTelegram";
 import './Header.css';
 
 
