60e2acd6fe1a516ced3d5cdd649947537ac9db53
YONSAN <112881127+yonyouyon@users.noreply.github.com>
2024-11-02 15:22:11 +1000

    add form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './components/hooks/useTelegram';
-
+import Header from './components/Header/Header';
+import {Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App()  {
-  const {onToogleButton, tg} = useTelegram();
+  const {onToggleButton, tg} = useTelegram();
 
   useEffect(() =>{
     tg.ready();
@@ -12,9 +15,11 @@ function App()  {
 
   return (
     <div className="App">
-      <button onClick={onToogleButton}>
-        toogle
-      </button>
+      <Header />
+      <Routes>
+        <Route index element={<ProductList />}/>
+        <Route path={'form'} element={<Form />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+import './Form.css';
+import { useTelegram } from "../../hooks/useTelegram";
+
+const Form = () => {
+    const [country, setCountry] = useState('');
+    const [sity, setSity] = useState('');
+    const [subject, setSubject] = useState('physucal');
+    const {tg} = useTelegram();
+
+    const onChangeCountry = (a) => {
+        setCountry(e.target.value)
+    }
+
+    const onChangeSity = (a) => {
+        setSity(e.target.value)
+    }
+
+    const onChangeSubject = (a) => {
+        setSubject(e.target.value)
+    }
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(() => {
+        if(!country || !city) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, city])
+
+    return (
+        <div className={'form'}>
+            <h3>введите ваши данные</h3>
+            
+            <input 
+                className={'input'} 
+                type='text' 
+                placeholder={'страна'}
+                value={country}
+                onChange={onChangeCountry}
+            />
+            <input 
+                className={'input'} 
+                type='text' 
+                placeholder={'город'}
+                value={country}
+                onChange={onChangeSity}
+            />
+
+            <select value={subject} onChange={onChangeSubject} className={'select'}>
+                <option value={'physical'}>Физ лицо</option>
+                <option value={'legal'}>Юр  лицо</option>
+            </select>
+        </div>
+    );
+};
+
+export default Form;
