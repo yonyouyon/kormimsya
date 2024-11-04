846468971d3855ae77ad1cbc7c24227ff74c6221
YONSAN <112881127+yonyouyon@users.noreply.github.com>
2024-11-04 23:54:15 +1000

    fix db

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,16 +3,12 @@ import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
 import { useTelegram } from "../hooks/useTelegram";
 
-// Подключаем библиотеку sqlite3
-const sqlite3 = require('sqlite3').verbose();
-const dbPath = './beats.db';  // Укажите путь к базе данных
-
 const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => acc + item.price, 0);
 }
 
 const ProductList = () => {
-    const [products, setProducts] = useState([]); // Список аудиофайлов из базы данных
+    const [products, setProducts] = useState([]); 
     const [addedItems, setAddedItems] = useState([]);
     const { tg, queryId } = useTelegram();
 
@@ -22,7 +18,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         };
-        fetch('http://85.119.146.179:8000/web-data', { // Замените на URL вашего сервера
+        fetch('http://85.119.146.179:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -38,31 +34,11 @@ const ProductList = () => {
         };
     }, [onSendData]);
 
-    // Функция для загрузки данных из базы данных SQLite
-    const fetchBeats = () => {
-        const db = new sqlite3.Database(dbPath);
-        db.all('SELECT id, genre, file_path AS title FROM beats', [], (err, rows) => {
-            if (err) {
-                console.error("Ошибка при чтении из базы данных:", err.message);
-                return;
-            }
-
-            const beats = rows.map(row => ({
-                id: row.id.toString(),
-                title: row.title,
-                genre: row.genre,
-                price: 5000, // Фиксированная цена, можно заменить на любое значение
-                description: `Genre: ${row.genre}`
-            }));
-
-            setProducts(beats); // Сохраняем в состояние
-        });
-        db.close();
-    };
-
-    // Загружаем данные при первом рендере компонента
     useEffect(() => {
-        fetchBeats();
+        fetch('http://localhost:8000/api/beats') 
+            .then(response => response.json())
+            .then(data => setProducts(data))
+            .catch(error => console.error('Ошибка загрузки данных:', error));
     }, []);
 
     const onAdd = (product) => {
